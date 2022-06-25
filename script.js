e84f8938fb738248637f09d3dbbe797566bc8922
Jacob <me@here.com>
2022-06-24 22:52:40 -0400

    Created method of generating windows using javascript.

--- script.js
+++ script.js
@@ -1,31 +1,56 @@
 document.body.onload = launchApp('terminal');
 
-const applications = {
-    terminal: initalizeTerminal
-}
-
 function createTray(name){
-    const tray = docmuent.createElement("div");
+    // Initializes the tray.
+    console.log("Generating tray: " + name);
+    const tray = document.createElement("div");
     tray.className = "row program-button-tray";
+    tray.setAttribute('id', 'terminal');
     
+    // Creates the tray name.
     const programName = document.createElement("div");
     programName.innerHTML = name;
     programName.className = "col container d-flex";
     tray.appendChild(programName);
 
+    // Creates the tray buttons.
     const buttonContainer = document.createElement("div");
     buttonContainer.className = "col container-fluid d-flex flex-row-reverse px-0";
-    const xButt = document.createElement("button")
+    const xButt = document.createElement("button");
+    xButt.className = "tray-buttons";
+    xButt.innerHTML = "X";
+    const minButt = document.createElement("button");
+    minButt.className = "tray-buttons";
+    minButt.innerHTML = "-";
+
+    buttonContainer.appendChild(xButt);
+    buttonContainer.appendChild(minButt);
+    tray.appendChild(buttonContainer);
 
     return tray;
 }
 
+function makeDraggable(app) {
+    
+}
+
 function initalizeTerminal() {
-    console.log("Initialising the terminal.");0
+    console.log("Initialising the terminal.");
     const terminal = document.createElement("div");
     terminal.setAttribute('id', 'terminal');
     terminal.className = "container terminal-window shadow";
     terminal.appendChild(createTray('Command Prompt'));
+
+    const termBody = document.createElement("div");
+    termBody.className = "row terminal-body container-fluid";
+    const bodyText = document.createElement("p");
+    bodyText.innerHTML = "Hello! I am a software engineer in the making! " + 
+    "Currently I am studying at FGCU and plan to graduate in Fall 2025.";
+    termBody.appendChild(bodyText);
+
+    terminal.appendChild(termBody);
+
+    document.body.appendChild(terminal);
 }
 
 function launchApp(application) {
@@ -33,6 +58,8 @@ function launchApp(application) {
     if (!!document.getElementById(application)) {
          console.log("App already open.");
     } else {
-        applications[application]();
+        if (application == "terminal") {
+            initalizeTerminal();
+        }
     }
 }
