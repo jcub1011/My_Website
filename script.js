143af334941031ebd067c798c32207aa9e165c0c
Jacob <me@here.com>
2022-06-26 19:30:48 -0400

    Migrated to setInterval.

--- script.js
+++ script.js
@@ -1,53 +1,5 @@
+document.body.onload = startTypeOuts();
 /*
-const text = 
-[`  _____             ____     ____     ____\n\
- /__ __/   /\\      / ___\\   / __ \\   |  __ \\\n\
-   | |    /  \\    / /      / /  \\ \\  | | / /\n\
-   | |   / /\\ \\  | |      | |    | | | |{ {\n\
- __/ /  / ____ \\  \\ \\____  \\ \\__/ /  | |_\\ \\\n\
-/___/  /_/    \\_\\  \\____/   \\____/   |_____/\n\n\
-McCormack`,];
-
-document.body.onload = typeOutText("nameHead", text[0]);
-
-function typeOutText(elementToPutTextIn, textToInsert) {
-    console.log("Setting up type out.");
-    const interval = 1; // Time between char in ms.
-
-    const element = document.getElementById(elementToPutTextIn);
-    
-    const textContainer = document.createElement('pre');
-    element.appendChild(textContainer);
-
-    console.log("Initializing call loop.");
-    textInsertCallLoop(elementToPutTextIn, textToInsert, interval, 0);
-}
-
-function textInsertCallLoop(elementToPutTextIn, textToInsert, interval, index) {
-    const element = document.getElementById(elementToPutTextIn).querySelector("pre");
-
-    if (index < textToInsert.length) {
-        element.innerHTML += textToInsert[index];
-
-        setTimeout(function() {
-            textInsertCallLoop(elementToPutTextIn, textToInsert, interval, index += 1)
-        }, interval);
-    } else {
-        console.log("Finished.");
-    }
-}
-
-function typeText() {
-
-}*/
-
-/**
- * Loop to make text appear char after char.
- * 
- * @param {Element} elmToType The element to type the text into. Inserts text into innerHTML.
- * @param {Text} textToInsert The text to be inserted.
- * @param {Number} index Index of the first character. Leave at default.
- */
 function typeOutLoop(elmToType, textToInsert, index = 0) {
     if (index < textToInsert.length) {
         elmToType.innerHTML += textToInsert[index];
@@ -57,11 +9,38 @@ function typeOutLoop(elmToType, textToInsert, index = 0) {
     } else {
         console.log("finished");
     }
+}*/
+
+/**
+ * Loop to make text appear char after char.
+ * 
+ * @param {Element} elmToType The element to type the text into. Inserts text into innerHTML.
+ * @param {Text} textToInsert The text to be inserted.
+ * @param {Number} index Index of the first character. Leave at default.
+ */
+function typeOutLoop(elmToType, textToInsert, speed = 1) {
+    let index = 0;
+    let interval = setInterval(function() {
+
+        for (i = 0; i < speed; i++) {
+            if (index < textToInsert.length) {
+                elmToType.innerHTML += textToInsert[index];
+                index += 1;
+            } else {
+                clearInterval(interval);
+                break;
+            }
+        }
+    }, 1);
+
+    console.log("Finished.");
 }
 
-startTypeOuts();
-
-function startTypeOuts() {
+/**
+ * Starts the typing out loop.
+ * @param {Boolean} serial Whether each element should be typed out one by one or at the same time.
+ */
+function startTypeOuts(serial = false) {
     // !IMPORTANT!, only put the 'type-out' class on elements containing text and nothing else.
     const elmsToTypeOut = document.getElementsByClassName("type-out");
     console.log(elmsToTypeOut);
@@ -74,7 +53,7 @@ function startTypeOuts() {
             const text = elm.innerHTML;
             elm.innerHTML = null;
             elm.style.display = 'inline';
-            typeOutLoop(elm, text);
+            typeOutLoop(elm, text, 3);
         }
     }
 }
