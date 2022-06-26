3f0c572d2add9cfc24c1ecc233cdb39401541017
Jacob <me@here.com>
2022-06-26 18:35:39 -0400

    New text typer implementation.

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-const text = [`
-  _____             ____     ____     ____\n\
+/*
+const text = 
+[`  _____             ____     ____     ____\n\
  /__ __/   /\\      / ___\\   / __ \\   |  __ \\\n\
    | |    /  \\    / /      / /  \\ \\  | | / /\n\
    | |   / /\\ \\  | |      | |    | | | |{ {\n\
  __/ /  / ____ \\  \\ \\____  \\ \\__/ /  | |_\\ \\\n\
 /___/  /_/    \\_\\  \\____/   \\____/   |_____/\n\n\
-McCormack`];
+McCormack`,];
 
 document.body.onload = typeOutText("nameHead", text[0]);
 
@@ -33,5 +34,47 @@ function textInsertCallLoop(elementToPutTextIn, textToInsert, interval, index) {
         }, interval);
     } else {
         console.log("Finished.");
+    }
+}
+
+function typeText() {
+
+}*/
+
+/**
+ * Loop to make text appear char after char.
+ * 
+ * @param {Element} elmToType The element to type the text into. Inserts text into innerHTML.
+ * @param {Text} textToInsert The text to be inserted.
+ * @param {Number} index Index of the first character. Leave at default.
+ */
+function typeOutLoop(elmToType, textToInsert, index = 0) {
+    if (index < textToInsert.length) {
+        elmToType.innerHTML += textToInsert[index];
+        setTimeout(function() {
+            typeOutLoop(elmToType, textToInsert, index + 1)
+        }, 1);
+    } else {
+        console.log("finished");
+    }
+}
+
+startTypeOuts();
+
+function startTypeOuts() {
+    // !IMPORTANT!, only put the 'type-out' class on elements containing text and nothing else.
+    const elmsToTypeOut = document.getElementsByClassName("type-out");
+    console.log(elmsToTypeOut);
+
+    if (elmsToTypeOut != null) {
+        console.log("Found elements to type");
+        for (i = 0; i < elmsToTypeOut.length; i++) {
+            console.log(elmsToTypeOut[i]);
+            const elm = elmsToTypeOut[i];
+            const text = elm.innerHTML;
+            elm.innerHTML = null;
+            elm.style.display = 'inline';
+            typeOutLoop(elm, text);
+        }
     }
 }
