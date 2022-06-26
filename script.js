25e5650e08b423161b11490135abe6283fbc59b9
Jacob <me@here.com>
2022-06-25 21:32:25 -0400

    Created header generating script.

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
+document.body.onload = displayHeader();
 
+function displayHeader() {
+    // Generates the header.
+    console.log("Generating header");
+    const head = document.createElement("div");
+    head.className = "header px-4 py-1";
 
+    const name = document.createElement("pre");
+    name.innerHTML = `
+  _____               ____     ____     ____\n\
+ /__ __/    /\\       / ___\\   / __ \\   |  __ \\\n\
+   | |     /  \\     / /      / /  \\ \\  | | / /\n\
+   | |    / /\\ \\   | |      | |    | | | |{ {\n\
+ __/ /   / ____ \\   \\ \\____  \\ \\__/ /  | |_\\ \\\n\
+/___/   /_/    \\_\\   \\____/   \\____/   |_____/\n\n\
+McCormack`;
+
+    head.appendChild(name);
+    document.body.appendChild(head);
+}
