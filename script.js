c34324bf73f468981ec5501f1d2d235ef3b99f8c
Jacob <me@here.com>
2022-06-25 22:31:52 -0400

    Changing the way text is displayed.

--- script.js
+++ script.js
@@ -1,21 +1,38 @@
-document.body.onload = displayHeader();
+document.body.onload = displayNameHeader();
 
-function displayHeader() {
+function drawText(objToDrawIn) {
+    console.log("Drawing text");
+    console.log(objToDrawIn);
+    const speed = 10; // Time in ms between each char.
+    const textToDraw = objToDrawIn.innerHTML;
+
+    objToDrawIn.innerHTML = null;
+    console.log(textToDraw);
+}
+
+function displayNameHeader() {
     // Generates the header.
     console.log("Generating header");
     const head = document.createElement("div");
     head.className = "header px-4 py-1";
+    head.setAttribute("id", "header");
 
-    const name = document.createElement("pre");
-    name.innerHTML = `
+    const fName = document.createElement("pre");
+    fName.style.color = "#E7F9A9";
+    fName.innerHTML = `
   _____             ____     ____     ____\n\
  /__ __/   /\\      / ___\\   / __ \\   |  __ \\\n\
    | |    /  \\    / /      / /  \\ \\  | | / /\n\
    | |   / /\\ \\  | |      | |    | | | |{ {\n\
  __/ /  / ____ \\  \\ \\____  \\ \\__/ /  | |_\\ \\\n\
-/___/  /_/    \\_\\  \\____/   \\____/   |_____/\n\n\
-McCormack`;
+/___/  /_/    \\_\\  \\____/   \\____/   |_____/`;
 
-    head.appendChild(name);
+    const lName = document.createElement("p");
+    lName.innerHTML = "McCormack";
+    head.appendChild(fName);
+    head.appendChild(lName);
+    
     document.body.appendChild(head);
+
+    drawText(document.querySelector("#header").fName);
 }
